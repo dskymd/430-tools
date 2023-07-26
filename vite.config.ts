637891b8fe6f3173559f4b5edfdc64fd36cf4dae
dskymd <dskymd@gmail.com>
2023-07-26 10:34:11 +0900

    add build base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // build: {
+  //   rollupOptions: {
+  //     "https://t430.com/_tools/"
+  //   },
+  // },
 })

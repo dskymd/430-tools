522baa453883b89d358da1faca7e4f250aa5f17d
dskymd <dskymd@gmail.com>
2023-07-26 10:25:35 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FileName from './components/FileName.vue'
+</script>
+
+<template>
+  <FileName />
+</template>
+
+<style scoped></style>
---
+++ src/components/FileName.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+import FileNameForm from './FileNameForm.vue'
+
+// defineProps<{ msg: string }>()
+// const count = ref(0)
+
+const updateCode = (val: string) => {
+  console.log(val)
+}
+</script>
+
+<template>
+  <p>ファイル一覧から商品IDごとに</p>
+  <FileNameForm @update:code="updateCode" />
+</template>
+
+<style scoped></style>
